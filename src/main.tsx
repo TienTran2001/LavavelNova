80e1d10b94d7c769fb4a3cd08813a2aa8e997b6e
Tiến Trần <tienco201@gmail.com>
2024-07-15 12:50:50 +0700

    fix ui deloy

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './index.scss';
 import { theme } from './utils/theme.ts';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import { MenuProvider } from './contexts/menuContext.tsx';
