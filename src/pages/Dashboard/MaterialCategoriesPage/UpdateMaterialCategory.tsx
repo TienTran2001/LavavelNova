36dd59693890d012194c0b996c5d6106676a20ed
Tiến Trần <tienco201@gmail.com>
2024-07-30 11:55:42 +0700

    update ui

--- src/pages/Dashboard/MaterialCategoriesPage/UpdateMaterialCategory.tsx
+++ src/pages/Dashboard/MaterialCategoriesPage/UpdateMaterialCategory.tsx
@@ -98,58 +98,60 @@ const UpdateMaterialCategory = () => {
                 />
               </div>
             </div>
-            <div className="flex flex-col w-2/4 bg-white shadow-sm rounded-[28px]">
-              <div className="px-8 py-5 ">
-                <InputForm
-                  labelClassName=""
-                  containerClassName=""
-                  label="Name*"
-                  register={register}
-                  id="name"
-                  placeholder="Enter name..."
-                  validate={{
-                    required: 'Name is required.',
-                  }}
-                  errors={errors}
-                />
-              </div>
-              <div className="px-8 py-5">
-                <SelectForm
-                  labelClassName=""
-                  containerClassName=""
-                  label="Price type*"
-                  value=""
-                  id="price_type"
-                  register={register}
-                  validate={{
-                    required: 'Price type not selected.',
-                  }}
-                  errors={errors}
-                >
-                  <option value="" className="!py-2" disabled>
-                    Choose price type
-                  </option>
-                  <option value={priceType.quantity.value}>
-                    {priceType.quantity.display}
-                  </option>
-                  <option value={priceType.metter.value}>
-                    {priceType.metter.display}
-                  </option>
-                </SelectForm>
-              </div>
-              <div className="flex justify-end px-8 py-5">
-                <LoadingButton
-                  loading={loading}
-                  sx={{
-                    backgroundColor: COLORS.primary500,
-                    textTransform: 'revert',
-                    borderRadius: '8px',
-                  }}
-                  variant="contained"
-                  type="submit"
-                >
-                  Update
-                </LoadingButton>
+            <div className="w-2/4">
+              <div className="flex flex-col bg-white shadow-sm rounded-[28px]">
+                <div className="px-8 py-5 ">
+                  <InputForm
+                    labelClassName=""
+                    containerClassName=""
+                    label="Name*"
+                    register={register}
+                    id="name"
+                    placeholder="Enter name..."
+                    validate={{
+                      required: 'Name is required.',
+                    }}
+                    errors={errors}
+                  />
+                </div>
+                <div className="px-8 py-5">
+                  <SelectForm
+                    labelClassName=""
+                    containerClassName=""
+                    label="Price type*"
+                    value=""
+                    id="price_type"
+                    register={register}
+                    validate={{
+                      required: 'Price type not selected.',
+                    }}
+                    errors={errors}
+                  >
+                    <option value="" className="!py-2" disabled>
+                      Choose price type
+                    </option>
+                    <option value={priceType.quantity.value}>
+                      {priceType.quantity.display}
+                    </option>
+                    <option value={priceType.metter.value}>
+                      {priceType.metter.display}
+                    </option>
+                  </SelectForm>
+                </div>
+                <div className="flex justify-end px-8 py-5">
+                  <LoadingButton
+                    loading={loading}
+                    sx={{
+                      backgroundColor: COLORS.primary500,
+                      textTransform: 'revert',
+                      borderRadius: '8px',
+                    }}
+                    variant="contained"
+                    type="submit"
+                  >
+                    Update
+                  </LoadingButton>
+                </div>
               </div>
             </div>
           </div>
