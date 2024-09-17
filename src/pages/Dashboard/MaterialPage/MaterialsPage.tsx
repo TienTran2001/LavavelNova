077ff32456aa79f0f101db99d3c596d596419993
tientran <tienco201@gmail.com>
2024-09-17 11:35:32 +0700

    feat: use search form (materials page)

--- src/pages/Dashboard/MaterialPage/MaterialsPage.tsx
+++ src/pages/Dashboard/MaterialPage/MaterialsPage.tsx
@@ -1,25 +1,22 @@
 // @react
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // @mui
-import SearchOffIcon from '@mui/icons-material/SearchOff';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 
 // @components
-import InputSearch from '~/components/Input/search/InputSearch';
+import ErrorBoundary from '~/components/Error/ErrorBoundary';
+import ErrorDataTable from '~/components/Error/ErrorComponent';
+import SearchBar from '~/pages/Dashboard/MaterialPage/components/SearchBar';
 import MaterialsTable from './components/MaterialsTable';
 
 // @utils
-import SearchCategory from '~/pages/Dashboard/MaterialPage/components/SearchCategory';
 import COLORS from '~/utils/colors';
 import currentPath from '~/utils/currentPath';
 
 export default function MaterialsPage() {
-  const navigate = useNavigate();
   return (
     <Box>
       <Typography
@@ -31,47 +28,30 @@ export default function MaterialsPage() {
       >
         Materials
       </Typography>
-      <Box
-        mt="50px"
-        display={'flex'}
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        <Box className="flex items-center gap-x-5">
-          <InputSearch
-            placeholder="Search name..."
-            className="shadow-sm"
-            query="q"
-          />
-          <SearchCategory />
-          <Tooltip title="Clear search">
-            <IconButton
-              aria-label="search"
-              sx={{ color: COLORS.gray400, p: '6px' }}
-              onClick={() => {
-                navigate(window.location.pathname, { replace: true });
+      <div className="mt-50 flex justify-between max-w-[1200px] w-full">
+        {/* ------------------ search ------------------ */}
+        <div className="w-[80%] ">
+          <SearchBar />
+        </div>
+        {/* ------------------ end search ------------------ */}
+        <div className="flex-shrink-0">
+          <Link to={currentPath.materials.create}>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: COLORS.primary500,
+                fontSize: 14,
+                fontWeight: 800,
+                color: 'white',
+                textTransform: 'none',
+                borderRadius: '4px',
               }}
             >
-              <SearchOffIcon />
-            </IconButton>
-          </Tooltip>
-        </Box>
-        <Link to={currentPath.materials.create}>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: COLORS.primary500,
-              fontSize: 14,
-              fontWeight: 800,
-              color: 'white',
-              textTransform: 'none',
-              borderRadius: '4px',
-            }}
-          >
-            Create Material
-          </Button>
-        </Link>
-      </Box>
+              Create Material
+            </Button>
+          </Link>
+        </div>
+      </div>
 
       {/* --------------------table------------------------ */}
       <Box mt={3}>
