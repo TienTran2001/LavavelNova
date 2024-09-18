5306abadb7c81736bd06c9a40466cb1ef12e922a
tientran <tienco201@gmail.com>
2024-09-18 17:44:46 +0700

    feat: add redirect if logged in

---
+++ app/_utils/redirectIfLoggedIn.ts
@@ -0,0 +1,19 @@
+import { useCheckIsHydrated } from '@/app/_hooks';
+import { useUserStore } from '@/app/_store/useUserStore';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+const redirectIfLoggedIn = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { user } = useUserStore();
+  const { isHydrated } = useCheckIsHydrated();
+
+  useEffect(() => {
+    if (isHydrated && user && pathname !== '/login') {
+      router.back();
+    }
+  }, [isHydrated, user, router]);
+};
+
+export default redirectIfLoggedIn;
